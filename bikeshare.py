a4ec458e30a0d046e97ab4609b6a0e3662c4b334
Subhan Siraj Patel <subhansp10@gmail.com>
2021-08-28 10:05:30 +0100

    chanded the output of the mean_travel_time by dividing by 60 so the outcome is in minutes and not seconds

--- bikeshare.py
+++ bikeshare.py
@@ -132,7 +132,7 @@ def trip_duration_stats(df):
 
     # TO DO: display mean travel time
     mean_travel_time = df['Trip Duration'].mean()
-    print("The mean travel time is", mean_travel_time)
+    print("The mean travel time is", mean_travel_time/60, "Minutes")
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
